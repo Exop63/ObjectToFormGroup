0e2ed599343f862b4a59e2957f7a34bc4a8e3999
Abdullah Bağlan <abdullah95baglan@gmail.com>
2021-02-05 03:24:42 +0300

    Update converter.ts
    Kasimin dokunuslari.

--- converter.ts
+++ converter.ts
@@ -1,28 +1,30 @@
- convert(data: any, ignor?: string[]): any {
+convert(data: any, ignor?: string[]): any {
     // console.log("Array = ", this.isArray(data), "Value: ", data);
     // console.log("Object = ", this.isObject(data), "Value: ", data);
 
     if (this.isArray(data)) {
       const array: any[] = [];
-      data.forEach(el => {
-        array.push(this.convert(el));
+      data.forEach((el) => {
+        array.push(this.convert(el,ignor));
       });
 
-      if (ignor) {
-        return array;
-      }
+      // if (ignor) {
+      //   return array;
+      // }
       return this.fb.array(array);
     } else if (this.isObject(data)) {
       let group = {};
-      Object.keys(data).forEach(key => {
-        if (ignor) {
-          if (ignor.some(item => item === key)) {
+      Object.keys(data).forEach((key) => {
+        if (ignor !== null && ignor !== undefined) {
+          if (ignor.some((item) => item === key)) {
             group[key] = [data[key]];
           } else {
-            group[key] = this.convert(data[key]);
+            console.log('CONVERT KEY 1',key);
+            group[key] = this.convert(data[key],ignor);
           }
         } else {
-          group[key] = this.convert(data[key]);
+          console.log('CONVERT KEY 2',key);
+          group[key] = this.convert(data[key],ignor);
         }
       });
 
